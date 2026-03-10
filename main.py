9b463cfe72b6daed2993b50e1fcfaa8ed1c33ffe
jeroo06 <jpalrol680@g.educaand.es>
2026-03-10 20:52:04 +0100

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 import discord
+import interaction
 
 from discord.ext import commands
 import datetime
 from dotenv import load_dotenv
 import io
 import asyncio
+
+from scripts.regsetup import description
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -25,7 +28,7 @@ CATEGORIAS_TICKETS = {
 }
 ID_CANAL_TRANSCRIPTS = 1479931001089163465
 
-# NUEVO: Lista de IDs de roles que pueden ver los tickets
+# Lista de IDs de roles que pueden ver los tickets
 ROLES_STAFF_IDS = [
     1479548860178108466
     # Puedes añadir más IDs aquí separados por comas
@@ -126,6 +129,9 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 
+
+#### COMANDO PARA ESTABLECER EL PANEL DE TICKES ####
+
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def ticketpanel(ctx):
@@ -145,6 +151,9 @@ async def ticketpanel(ctx):
 
     await canal.send(embed=embed_ticketpanel, view=TicketView())
 
+
+
+#### COMANDO PARA RENOMBRAR UN TICKET ####
 
 @bot.command()
 @commands.check_any(commands.has_role(1479548860178108466), commands.has_permissions(administrator=True))
@@ -166,6 +175,9 @@ async def rename(ctx, *, new_name: str):
         await ctx.send("An error occurred")
 
 
+
+#### COMANGO PARA ELIMINAR CIERTOS MENSAJES ####
+
 @bot.command()
 @commands.has_permissions(manage_messages=True)
 async def purge(ctx, amount: int):
@@ -177,6 +189,9 @@ embed_errorchanel = discord.Embed(
         description="This channel is not a ticket channel."
     )
 
+
+
+#### COMANDO PARA AÑADIR A UN USUARIO A UN TICKET ####
 
 @bot.command()
 @commands.check_any(commands.has_role(1479548860178108466), commands.has_permissions(administrator=True))
@@ -198,6 +213,9 @@ async def add(ctx, new_user: discord.Member):
         await ctx.send(embed=embed_errorchanel, delete_after=5)
 
 
+
+#### COMANDO PARA ELIMINAR A UN USUARIO DE UN TICKET ####
+
 @bot.command()
 @commands.check_any(commands.has_role(1479548860178108466), commands.has_permissions(administrator=True))
 async def remove(ctx, user_deleted: discord.Member):
@@ -214,5 +232,103 @@ async def remove(ctx, user_deleted: discord.Member):
     else:
         await ctx.send(embed=embed_errorchanel, delete_after=5)
 
+
+
+#### COMANDO PARA CERRAR LOS TICKETS ####
+
+@bot.command()
+@commands.check_any(commands.has_role(1479548860178108466), commands.has_permissions(administrator=True))
+async def close(ctx):
+    if ctx.channel.category_id in CATEGORIAS_TICKETS.values():
+        await ctx.message.delete()
+
+        embed_transcript = discord.Embed(
+            description=f"Channel closed. Generating transcript...",
+            color=discord.Color.purple(),
+        )
+        await ctx.send(embed=embed_transcript)
+        log_content = f"Transcript for {ctx.channel.name}\n"
+        log_content += f"Closed by (Command): {ctx.author} ({ctx.author.id})\n"
+        log_content += "-" * 30 + "\n"
+
+        async for message in ctx.channel.history(limit=None, oldest_first=True):
+            time = message.created_at.strftime("%Y-%m-%d %H:%M:%S")
+            log_content += f"[{time}] {message.author}: {message.content}\n"
+
+        canal_logs = bot.get_channel(ID_CANAL_TRANSCRIPTS)
+        if canal_logs:
+            file = discord.File(
+                io.BytesIO(log_content.encode()),
+                filename=f"transcript-{ctx.channel.name}.txt"
+            )
+            await canal_logs.send(content=f"Ticket closed via command: **{ctx.channel.name}**", file=file)
+
+        await asyncio.sleep(3)
+        await ctx.channel.delete()
+    else:
+        await ctx.send(embed=embed_errorchanel, delete_after=5)
+
+
+
+#### CONFIGURACION DE ANTISPAM ####
+canal_prohibido_id = 1480953985203572910
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+    canal = bot.get_channel(canal_prohibido_id)
+
+    if canal:
+
+        await canal.purge(limit=10, check=lambda m: m.author == bot.user)
+
+
+        embed_info = discord.Embed(
+            title="CANAL ANTI-SPAM BY JRR'S STUDIO BOT",
+            description="Cualquier persona que envíe un mensaje por aquí será aislado automáticamente durante 1 semana.",
+            color=discord.Color.purple(),
+        )
+        embed_info.set_author(
+            name="jrr's studio",
+            icon_url="https://media.discordapp.net/attachments/1340021249811939382/1480612688068608132/jrrs_studio.png"
+        )
+
+        await canal.send(embed=embed_info)
+        print("Mensaje informativo enviado al canal prohibido.")
+
+
+@bot.event
+async def on_message(message):
+
+    if message.author == bot.user:
+        return
+
+
+    if message.channel.id == canal_prohibido_id:
+        usuario = message.author
+        ahora = datetime.datetime.now(datetime.timezone.utc)
+
+
+        try:
+            duracion = datetime.timedelta(weeks=1)
+            await usuario.timeout(duracion, reason="Spam detection by jrr's studio bot")
+        except Exception as e:
+            print(f"Error al aislar: {e}")
+
+
+        hace_una_hora = ahora - datetime.timedelta(hours=1)
+        try:
+            await message.channel.purge(
+                limit=100,
+                after=hace_una_hora,
+                check=lambda m: m.author == usuario
+            )
+        except Exception as e:
+            print(f"Error al aislar: {e}")
+
+    await bot.process_commands(message)
 
 bot.run(TOKEN)
